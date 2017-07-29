67e96b5f9ab07a0f0130124c933855486ff9b0ad
Student <alex.escobido@jmc.educ.ph>
2017-07-29 12:41:20 +0800

    commiting my changes alexescobido

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -36,9 +36,9 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(70, 41);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(178, 13);
+            this.label1.Size = new System.Drawing.Size(199, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "this is my contribution alex escobido ";
+            this.label1.Text = "this is my contribution fgh+alex escobido ";
             // 
             // Form1
             // 
