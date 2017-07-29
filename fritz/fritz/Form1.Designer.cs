fbded44490b01c5762b0bfed9e97fdf6a0678a8b
fritz <fritz.barnuevo@jmc.edu.ph>
2017-07-29 12:27:51 +0800

    Commiting my changes RODGIE FRITZ BARNUEVO

--- fritz/fritz/Form1.Designer.cs
+++ fritz/fritz/Form1.Designer.cs
@@ -36,9 +36,9 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(69, 49);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(204, 13);
+            this.label1.Size = new System.Drawing.Size(216, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "this is my contribution FRITZ BARNUEVO";
+            this.label1.Text = "aathis is my contribution FRITZ BARNUEVO";
             // 
             // Form1
             // 
